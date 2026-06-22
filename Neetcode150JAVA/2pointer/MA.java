a063bf3a9a34ec33ee97dd0fc979db72389e11d9
danishjk2156 <danishjk28052005@gmail.com>
2026-06-22 15:34:29 +0530

    completed 2 problems and sliding window

---
+++ Neetcode150JAVA/2pointer/MA.java
@@ -0,0 +1,29 @@
+public class MA {
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+        int result = maxArea(height);
+
+        System.out.println("Maximum Water Container Area: " + result);
+    }
+}
